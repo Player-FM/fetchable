21449ffcffb1381f8966d303d74fc7d7c884d0d2
Mahemoff <michael@mahemoff.com>
2014-04-19 11:03:23 +0100

    try travis integration

--- test/dummy/config/initializers/testdata.rb
+++ test/dummy/config/initializers/testdata.rb
@@ -1,7 +1,8 @@
 module Dummy
 
-  TESTDATA_HOST = 'http://testdata.biz'
-  TESTFILE_HOST = 'http://testdata.biz/file.php'
+  #TESTDATA_HOST = "http://testdata.biz"
+  TESTDATA_HOST = "http://testdata.player.fm"
+  TESTFILE_HOST = "#{TESTDATA_HOST}/file.php"
 
   def self.test_file(params={})
     uri = Addressable::URI.parse(TESTFILE_HOST)
