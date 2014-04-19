cf2e676f48af93c4c5f790136465115fb2cb3b83
Mahemoff <michael@mahemoff.com>
2014-04-19 22:17:16 +0100

    configurable testdata server

--- test/dummy/config/initializers/testdata.rb
+++ test/dummy/config/initializers/testdata.rb
@@ -1,7 +1,7 @@
 module Dummy
 
-  #TESTDATA_HOST = "http://testdata.biz"
-  TESTDATA_HOST = "http://testdata.player.fm"
+  TESTDATA_HOST = ENV['TESTDATA_HOST'] || 'http://testdata.player.fm'
+
   TESTFILE_HOST = "#{TESTDATA_HOST}/file.php"
 
   def self.test_file(params={})
